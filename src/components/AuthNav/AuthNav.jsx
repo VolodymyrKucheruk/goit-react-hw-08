a02d2ea91c54698a2986bed9a38fec58baf801c5
VolodymyrKucheruk <voloda.2014a@gmail.com>
2024-03-10 05:25:28 +0100

    modefied6

--- src/components/AuthNav/AuthNav.jsx
+++ src/components/AuthNav/AuthNav.jsx
@@ -1,6 +1,6 @@
 import { NavLink } from "react-router-dom";
 import css from "./AuthNav.module.css";
-import { GrLogin } from "react-icons/gr";
+
 
 
 export const AuthNav = () => {
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,5 @@
 import { Suspense } from "react";
 import { Outlet } from "react-router-dom";
-import { Toaster } from "react-hot-toast";
 import { AppBar } from "./AppBar/App.Bar";
 
 export const Layout = () => {
@@ -19,7 +18,6 @@ export const Layout = () => {
       <Suspense fallback={null}>
         <Outlet />
       </Suspense>
-      <Toaster position="top-right" reverseOrder={false} />
     </div>
   );
 };
