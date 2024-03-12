1ef6e8cd87fc1319e0e038ef29daa48343ce5b7f
VolodymyrKucheruk <voloda.2014a@gmail.com>
2024-03-12 19:56:12 +0100

    mod9

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,6 +1,7 @@
 import css from "./NotFoundPage.module.css";
 import DocumentTitle from "../../components/DocumentTitle";
 import image from "../../assets/image.png";
+import { Link } from "react-router-dom";
 
 const NotFoundPage = () => {
   return (
@@ -9,6 +10,9 @@ const NotFoundPage = () => {
       <DocumentTitle>NotFound</DocumentTitle>
       <img className={css.image} src={image} alt="Not found image" />;
       <h1 className={css.text}>Not found, please try again...</h1>
+      <Link className={css.link} to="/">
+        Back to home page
+      </Link>
     </>
   );
 };
